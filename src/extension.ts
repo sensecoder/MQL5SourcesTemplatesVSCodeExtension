ee535ce9a4898519675a5f887181f493d658bed8
sensecoder <sensecoding@gmail.com>
2022-06-16 00:45:05 +0500

    final version 1.0

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,6 @@ import { Settings } from './modules/settings';
 import { SettingsHandler } from './modules/settings_handler';
 import { TemplateHandler } from './modules/template_handler';
 import { UserChoiceHandler } from './modules/user_choice_handler';
-import { Insertor } from './modules/v0.1/insertor';
 
 // States of the automaton
 const enum AutomatonState {
@@ -22,14 +21,14 @@ const enum Message {
 	fileCreated,
 	fileAccepted,
 	userMadeChoice,
-	waitEditor,
+	awaitEditor,
 	templateApplied,
 	abortAndRestart
 }
 
-// ?Listener subscription
+// Listener subscription
 let subscription: vscode.Disposable;
-let anotherSubscription: vscode.Disposable;
+let editorAwaitSubscription: vscode.Disposable;
 
 // Extension context
 let extContext: vscode.ExtensionContext;
@@ -40,26 +39,23 @@ let fileName: string = '';
 // Automaton state
 let automatonState: AutomatonState;
 
+// Result template text
+let templateText = '';
+
+// File created editor param
+let newEditorColumn: vscode.ViewColumn | undefined;
+let newDocument: vscode.TextDocument | undefined;
+
 export function activate(context: vscode.ExtensionContext) {
-
 	console.log('Extension "mql5SourcesTemplate" is now active!');
-
-	// let disposable = vscode.commands.registerCommand('mql5SourcesTemplate.helloWorld', () => {
-	// 	vscode.window.showInformationMessage('Hello World from MQL5SourcesTemplates!');
-	// });
-	// context.subscriptions.push(disposable);
-
 	extContext = context;
 	toDo(Message.startAutomaton);
-	// console.log(`Automaton state: ${automatonState}`);	
-
-	//console.log('Current dir='+__dirname);
-	// vscode.window.showInformationMessage('MQL5 Sources Template Activated!');
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
 	subscription.dispose(); // stop listening
+	editorAwaitSubscription.dispose();
 }
 
 function toDo(message: Message, ...argsArr: any[]) {
@@ -86,23 +82,22 @@ function toDo(message: Message, ...argsArr: any[]) {
 			break;
 		case Message.userMadeChoice:
 				if (automatonState === AutomatonState.waitingUserChoice) {
-					// Отрисовка шаблона в редакторе файла:
-					// console.log('receiverUserOptions: ' + fileName);
-					automatonState = AutomatonState.applyingTemplate;
 					let settings = new Settings(argsArr[0]);
 					let templateHandler = new TemplateHandler(settings);
-					let text = templateHandler.renderText();
-					console.log('This a text = ' + text);
-					const insertor = new Insertor(argsArr[0], fileName);
-					insertor.applyTemplate();
-					vscode.window.showInformationMessage('Template Created!');
-					console.log('Template Successfully Created!');
-					toDo(Message.templateApplied);
-					//return insertor.applyTemplate();
+					templateText = templateHandler.renderText();
+					// console.log(templateText);
+					if (newDocument) {
+						vscode.window.showTextDocument(newDocument, newEditorColumn);
+					}
+					editorAwaitSubscription = vscode.window.onDidChangeActiveTextEditor(activeEditorCheck);
+					automatonState = AutomatonState.waitingEditor;
 				}
 			break;
 		case Message.abortAndRestart:
 				fileName = '';
+				templateText = '';
+				newEditorColumn = undefined;
+				newDocument = undefined;
 				automatonState = AutomatonState.waitingFileCreation;
 			break;
 		case Message.templateApplied:
@@ -121,24 +116,12 @@ let listenerOfFileCreation = function(event: vscode.FileCreateEvent) {
 	file = event.files[0].fsPath;
 	console.log('New created file detected: ' + file.substring(file.lastIndexOf('/') + 1));
 	toDo(Message.fileCreated, file);
-/*
-	
-
-	
-	// const templateCreator = new Creator(file, extContext);
-	// templateCreator.createTemplate();
-*/
 };
-
-function userMakeChoice(settings: any) {
-	const userChoice = new UserChoiceHandler(settings, extContext);
-   let listener = userChoiceReciever;
-   userChoice.chooseOption(listener, fileName);
-}
 
 function checkFile() {
 	const settingsHandler = new SettingsHandler();
 	let settings = settingsHandler.getSettingsForFile(fileName);
+	editorAwaitSubscription = vscode.window.onDidChangeActiveTextEditor(newEditorCheck);
 
 	if (!settings) {
 		console.log('extension.checkFile(): This file is not supported!');
@@ -170,17 +153,56 @@ let userChoiceReciever = function(message: string, selectedOptions: any)
 	}
 };
 
-let listenerOfEditorIsVisible = function (event: vscode.TextEditor[]) {
-	let pos = new vscode.Position(0,0);
-	let activeEditor = vscode.window.activeTextEditor;
-	if (!activeEditor) {
-		vscode.window.showErrorMessage('Active editor not exist!');
+let newEditorCheck = function(editor: vscode.TextEditor | undefined) {
+	if (editor === undefined) {//activeEditor) {
+		// vscode.window.showErrorMessage('Active editor not exist!');
 		return;
 	}
-	activeEditor.edit( e => {
-		e.insert(pos,'ypa!!!');
-		//vscode.TextEdit.insert(pos,'ypa!!!');
-		vscode.window.showInformationMessage('is work?');
+	if (editor.document.fileName === fileName) {
+		// vscode.window.showInformationMessage('New editor detected!');
+		newEditorColumn = editor.viewColumn;
+		newDocument = editor.document;
+		editorAwaitSubscription.dispose();
+	} else {
+		console.error(`extension.newEditorCheck(): Something goes wrong! A new editor has been created without matching file name! Abort process!`);
+		editorAwaitSubscription.dispose();
+		toDo(Message.abortAndRestart);
+	}
+};
+
+let activeEditorCheck = function (editor: vscode.TextEditor | undefined) {
+	if (editor === undefined) {//activeEditor) {
+		// vscode.window.showErrorMessage('Active editor not exist!');		
+		return;
+	}
+	if (editor.document.fileName !== fileName) {
+		// Need to find according document
+		// vscode.window.showInformationMessage('need to find editor!');
+		let documents = vscode.workspace.textDocuments;
+		if (documents.length === 0) {
+         console.error('extension.activeEditorCheck(..): No one open documents here!');
+			editorAwaitSubscription.dispose();
+			toDo(Message.abortAndRestart);
+      } else {
+         // console.log('this file = ' + this.file);
+         for (let i = 0; i < documents.length; i++) {
+            //document = documents[i];
+            // console.log('editor[' + i + '] = ' + editor.document.fileName);
+				if (documents[i].fileName === fileName) {               
+               vscode.window.showTextDocument(documents[i], newEditorColumn);
+               console.log('extension.activeEditorCheck(..): I will show you document!');
+               return;
+					// return vscode.window.activeTextEditor;
+            }
+         }
+      }
+		return;
+	}
+	let pos = new vscode.Position(0,0);
+	editor.edit( e => {
+		e.insert(pos,templateText);
+		// vscode.window.showInformationMessage('is work?');
 	});
-	anotherSubscription.dispose();
+	editorAwaitSubscription.dispose();
+	toDo(Message.templateApplied);
 };
